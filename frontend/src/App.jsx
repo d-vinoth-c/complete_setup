cd31a213941373bf3d17bb82ea742ae799aacf05
d-vinoth-c <vinovinth19@gmail.com>
2025-07-17 15:47:20 +0530

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const FUNCTION_URL = 'https://us-central1-YOUR_PROJECT.cloudfunctions.net/greet_user';
+
+function App() {
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+
+  const callGET = async () => {
+    const res = await axios.get(`${FUNCTION_URL}?name=${name}`);
+    setMessage(res.data.message);
+  };
+
+  const callPOST = async () => {
+    const res = await axios.post(FUNCTION_URL, { name });
+    setMessage(res.data.message);
+  };
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>React + Firebase + Python Cloud Function</h1>
+      <input
+        type="text"
+        placeholder="Enter name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <div style={{ marginTop: 10 }}>
+        <button onClick={callGET}>Call GET</button>
+        <button onClick={callPOST}>Call POST</button>
+      </div>
+      <h3>{message}</h3>
+    </div>
+  );
+}
+
+export default App;
